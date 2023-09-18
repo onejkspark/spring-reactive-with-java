277fe159d6b511acf231326068109eeaaeb5d72a
spark <pnw123456@icloud.com>
2023-09-18 21:35:50 +0900

    build: project setting

---
+++ reactivebook/chapter0/src/main/java/com/example/chapter0/Chapter0Application.java
@@ -0,0 +1,13 @@
+package com.example.chapter0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter0Application.class, args);
+	}
+
+}
--- reactivebook/chapter1/src/test/java/com/example/chapter1/Chapter1ApplicationTests.java
+++ reactivebook/chapter1/src/test/java/com/example/chapter1/Chapter1ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.reactive;
+package com.example.chapter1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ReactiveApplicationTests {
+class Chapter1ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
